e5fd1ad1e6e67aeb62f38c3fa88f4965747841b3
JUANES5545 <juanes.reich@gmail.com>
2024-11-17 23:53:48 -0500

    neww

---
+++ cypress/cypress/e2e/ghost_4-5/--run-all.cy.js
@@ -0,0 +1,12 @@
+// cypress/e2e/ghost_4-5/--run--all.cy.js
+
+// Import test files
+import './E002-view_date_user_profile.cy.js';
+import './E006-add_new_member.cy.js';
+import './E008-create_page.cy.js';
+import './E011-manage_tags.cy.js';
+import './E016-modify_site_name.cy.js';
+
+describe('Run All Tests', () => {
+    // This block will run all the imported tests
+});
